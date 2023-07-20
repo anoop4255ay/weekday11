b3a69bcf775bdce09c5776adf8671ee063d8433c
Anoop <anoop4255ay@gmail.com>
2023-07-20 11:14:36 -0400

    create new project

---
+++ src/H.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Home(){
+    return(
+        <section>
+        <div>it is a perfect person.</div>
+        <h2>that is a good boy</h2>
+        </section>
+    )
+}
+export default Home
+
+export function Myhome()
+{
+    return(
+        <h1>JAI SHRI RADHE</h1>
+    )
+}
